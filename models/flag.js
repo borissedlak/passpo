f8f87f8533148a8758ef6ed616740fa212453caa
Boris <bsedlak01@gmail.com>
2017-10-20 16:46:34 +0200

    Added flag model

---
+++ models/flag.js
@@ -0,0 +1,25 @@
+// Load required packages
+var mongoose = require('mongoose');
+var User = require('./user');
+
+// Define our beer schema
+var FlagSchema   = new mongoose.Schema({
+    pos: {
+        origin: {
+            lat: Number,
+            long: Number
+        },
+        goal: {
+            lat: Number,
+            long: Number
+        },
+        current: {
+            lat: Number,
+            long: Number
+        }
+    },
+    owner: [{ type: Schema.Types.ObjectId, ref: 'User' }]
+});
+
+// Export the Mongoose model
+module.exports = mongoose.model('Flag', FlagSchema);
