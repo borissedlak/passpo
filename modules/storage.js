ce5a376b87ba5ab14c6e74178a3155e8e3ecc315
Boris <mt151105@fhstp.ac.at>
2017-12-13 09:52:08 +0100

    changed statement for db decision

--- modules/storage.js
+++ modules/storage.js
@@ -1,16 +1,20 @@
 var User = require('../models/user');
 var mongoose = require('mongoose');
+var connectionString;
 
-var config = JSON.parse(process.env.APP_CONFIG);
-var mongoPassword = 'SuperSecure55#!';
-var evennodeConnectionString = "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + config.mongo.hostString;
-var mLabConnectionString = 'mongodb://Basta55:HcAftPbye2@ds121575.mlab.com:21575/api';
+if(process.env.APP_CONFIG != null){
+	var config = JSON.parse(process.env.APP_CONFIG);
+	var mongoPassword = 'SuperSecure55#!';
+	connectionString = "mongodb://" + config.mongo.user + ":" + encodeURIComponent(mongoPassword) + "@" + config.mongo.hostString;
+}
+else{
+	connectionString = 'mongodb://Basta55:HcAftPbye2@ds121575.mlab.com:21575/api';
+}
 
 module.exports = {
 	// Do I need the connection anyhow?
     connect: function () {
-		var correctString = (config) ? evennodeConnectionString : mLabConnectionString;
-		mongoose.connect(correctString)
+		mongoose.connect(connectionString)
 	},
 	connected: function () {
 		return (mongoose.connection.readyState == 1);
