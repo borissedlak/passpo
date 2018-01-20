38a5fbc4f504c5c0714b6b4991c92fae09b7cf42
Boris <mt151105@fhstp.ac.at>
2018-01-20 15:38:15 +0100

    debugging profilePictures folder

--- server.js
+++ server.js
@@ -167,7 +167,7 @@ app.post('/upload', function (req, res) {
 					}
 				});*/
 
-				imageFile.mv(`./profile_pictures/${pictureID}.jpg`, function (err) {
+				imageFile.mv(`./profilePictures/${pictureID}.jpg`, function (err) {
 					if (err) {
 						console.log("couldn't move file", err);
 						return res.status(500).send(err);
